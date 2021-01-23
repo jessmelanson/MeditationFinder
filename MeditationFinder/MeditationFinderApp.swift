64cb8278dea91d97b0e165b3dc751d3485f49e2a
Jess Melanson <jessica_melanson@yahoo.com>
2021-01-23 10:54:15 -0500

    Initial Commit

---
+++ MeditationFinder/MeditationFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationFinderApp.swift
+//  MeditationFinder
+//
+//  Created by Jessica Melanson on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MeditationFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
